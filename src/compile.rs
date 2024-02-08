69438109729ce268cd1cfc190793e748fbb74eb3
umar <umarzein1314@gmail.com>
2024-02-08 14:00:32 +0700

    cargo check is good

--- src/compile.rs
+++ src/compile.rs
@@ -31,19 +31,20 @@ pub struct Codegen<'a, 'ctx> {
     pub program_name: String,
 }
 
-impl<'a, 'ctx> Codegen<'a, 'ctx> {
-    fn add_function(&mut self, name: &'ctx str, ftype: FunctionType<'ctx>, linkage: Option<Linkage>)->FunctionValue<'ctx>{
+impl<'a, 'ctx> Codegen<'a, 'ctx> where 'ctx:'a{
+    fn add_function(&self, name: &'ctx str, ftype: FunctionType<'ctx>, linkage: Option<Linkage>)->FunctionValue<'ctx>{
         let f = self.module.add_function(name, ftype, linkage);
         //self.functions.insert(name, f);    
         return f;
 
     }
-    fn pair_to_type(&self, pair: Pair<Rule>) -> BasicTypeEnum{
+    #[inline]
+    fn pair_to_type(context: &'ctx Context, pair: Pair<Rule>) -> BasicTypeEnum<'ctx>{
         match pair.as_rule(){
-            Rule::integer_type => self.context.i64_type().into(),
-            Rule::real_type => self.context.f64_type().into(),
-            Rule::bool_type => self.context.bool_type().into(),
-            Rule::pointer_type => self.pair_to_type(pair.into_inner().next().unwrap()).ptr_type(AddressSpace::default()).into(),
+            Rule::integer_type => context.i64_type().into(),
+            Rule::real_type => context.f64_type().into(),
+            Rule::bool_type => context.bool_type().into(),
+            Rule::pointer_type => Self::pair_to_type(context, pair.into_inner().next().unwrap()).ptr_type(AddressSpace::default()).into(),
             _ => unreachable!()
         }
     }
@@ -65,7 +66,7 @@ impl<'a, 'ctx> Codegen<'a, 'ctx> {
         todo!()
     }
     
-    fn process_parameter(&self, pair:Pair<Rule>)->(bool,bool,Vec<String>,BasicTypeEnum){
+    fn process_parameter(context: &'ctx Context, pair:Pair<Rule>)->(bool,bool,Vec<String>,BasicTypeEnum<'ctx>){
         if pair.as_rule()!=Rule::parameter{
             unreachable!()
         }
@@ -73,7 +74,7 @@ impl<'a, 'ctx> Codegen<'a, 'ctx> {
         let mut invar = false;
         let mut outvar = false;
         let mut vars: Vec<_> = vec![];
-        let mut partype = self.context.i8_type().as_basic_type_enum();
+        let mut partype = context.i8_type().as_basic_type_enum();
         while let Some(x) = i.next(){
             match x.as_rule(){
                 Rule::param_io => {
@@ -84,7 +85,7 @@ impl<'a, 'ctx> Codegen<'a, 'ctx> {
                 Rule::type_field => {
                     for type_decl in x.into_inner(){
                         let mut k = type_decl.into_inner().rev();
-                        partype = self.pair_to_type(k.next().unwrap());
+                        partype = Self::pair_to_type(&context, k.next().unwrap());
                         let var_ptrs: Vec<_> = k.map(|x|x.as_str().to_string()).collect();
                         vars.extend(var_ptrs);
                     }
@@ -94,12 +95,36 @@ impl<'a, 'ctx> Codegen<'a, 'ctx> {
         }
         return (invar,outvar,vars,partype)
     }
-    fn compile_pest_output(&mut self, pair: Pair<Rule>){
+    fn compile_pest_output(&self, pair: Pair<Rule>){
         use Rule as R;
         match pair.as_rule(){
-            R::COMMENT => (),
-            R::WHITESPACE => (),
-            R::procedure_def => todo!(),
+            R::procedure_def => {
+                let mut i = pair.into_inner();
+                let name = i.next().unwrap().as_str();//
+                let mut intype_blueprint: Vec<(bool,bool,String,BasicTypeEnum)> = vec![];
+                let mut intypes: Vec<BasicMetadataTypeEnum> = vec![];
+                let mut outtype = self.context.i8_type().as_basic_type_enum();
+                let parameters: Vec<_> = i.clone().filter(|x|x.as_rule()==R::parameter)
+                    .map(|x|Self::process_parameter(&self.context,x)).collect();
+                for (inv,outv, vars, typ) in parameters{
+                    for var in vars{
+                        intype_blueprint.push((inv,outv,var.clone(),typ));
+                        intypes.push(typ.into());
+                    }
+                }
+                let context = self.context;
+                let module = self.module;
+                let functype = context.void_type().clone().fn_type(&intypes, false).clone();
+                let func = self.module.add_function(name, functype, None);
+                let funcblock = self.context.append_basic_block(func, "entry");
+                self.builder.position_at_end(funcblock);
+                while let Some(x) = i.next(){
+                    match x.as_rule(){
+                        R::konstanta | R::kamus | R::algoritma => self.compile_pest_output(x),
+                        _ => unreachable!()
+                    }
+                }
+            },
             R::function_def => {
                 let mut i = pair.into_inner();
                 let name = i.next().unwrap().as_str();//
@@ -107,19 +132,26 @@ impl<'a, 'ctx> Codegen<'a, 'ctx> {
                 let mut intypes: Vec<BasicMetadataTypeEnum> = vec![];
                 let mut outtype = self.context.i8_type().as_basic_type_enum();
                 let parameters: Vec<_> = i.clone().filter(|x|x.as_rule()==R::parameter)
-                    .map(|x|self.process_parameter(x)).collect();
+                    .map(|x|Self::process_parameter(&self.context,x)).collect();
                 for (inv,outv, vars, typ) in parameters{
                     for var in vars{
                         intype_blueprint.push((inv,outv,var.clone(),typ));
                         intypes.push(typ.into());
                     }
                 }
-                let outtyp = self.pair_to_type(i.clone().find(|x|x.as_rule()==R::typename).unwrap());
-                let functype = outtyp.fn_type(&intypes, false);
+                let typename = i.clone().find(|x|x.as_rule()==R::typename).unwrap();
+                let context = self.context;
+                let module = self.module;
+                let outtyp = Self::pair_to_type(&context, typename);
+                let functype = outtyp.clone().fn_type(&intypes, false).clone();
                 let func = self.module.add_function(name, functype, None);
-                self.context.append_basic_block(func, "entry");
+                let funcblock = self.context.append_basic_block(func, "entry");
+                self.builder.position_at_end(funcblock);
                 while let Some(x) = i.next(){
-                    self.compile_pest_output(x);
+                    match x.as_rule(){
+                        R::konstanta | R::kamus | R::algoritma => self.compile_pest_output(x),
+                        _ => unreachable!()
+                    }
                 }
 
             },
@@ -130,7 +162,7 @@ impl<'a, 'ctx> Codegen<'a, 'ctx> {
                 let mainf_entry = self.context.append_basic_block(main_fn, "mainf_entry");
                 self.builder.position_at_end(mainf_entry);
                 let mut i = pair.into_inner(); 
-                self.program_name = i.next().unwrap().as_str().to_string();
+                let program_name = i.next().unwrap().as_str().to_string();
                 let _ = i.map(|p|self.compile_pest_output(p)).collect::<Vec<()>>();
             },
             R::konstanta => {
@@ -139,7 +171,11 @@ impl<'a, 'ctx> Codegen<'a, 'ctx> {
                 for ctf in j{
                     let mut k = ctf.into_inner();
                     let ident = k.next().unwrap().as_str();
-                    let ty = self.pair_to_type(k.next().unwrap());
+                    let module = self.module;
+                    let context = self.context;
+                    let ty = Self::pair_to_type(&context, k.next().unwrap());
+                    //let ftype = ty.fn_type(&[], false);
+                    //let func = module.add_function("name", ftype, None);
                     let var_ptr = self.builder.build_alloca(ty, ident).unwrap();
                     let val = k.next().unwrap();
                     
@@ -150,7 +186,8 @@ impl<'a, 'ctx> Codegen<'a, 'ctx> {
                 let j = pair.into_inner();
                 for type_decl in j{
                     let mut k = type_decl.into_inner().rev();
-                    let ty = self.pair_to_type(k.next().unwrap());
+                    let context = self.context;
+                    let ty = Self::pair_to_type(&context, k.next().unwrap());
                     let var_ptrs: Vec<_> = k.map(|x|self.builder.build_alloca(ty, x.as_str()).unwrap()).collect();
                 }
 
@@ -161,7 +198,7 @@ impl<'a, 'ctx> Codegen<'a, 'ctx> {
             _ => unreachable!()
         }
     }
-    pub fn add_printf(&mut self){
+    pub fn add_printf(&self){
         let int32 = self.context.i32_type();
         let int8ptr = self.context.i8_type().ptr_type(AddressSpace::default());
         let printf_types = int32.fn_type(&[int8ptr.into()], true);
